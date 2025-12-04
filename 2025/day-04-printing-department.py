cab91c711e24ae51fe99dbc30b2ea2563af8b175
Jack Osmond <james.osmond@dhsc.gov.uk>
2025-12-04 11:15:15 +0000

    Add day 4 part 2 answer

--- 2025/day-04-printing-department.py
+++ 2025/day-04-printing-department.py
@@ -12,14 +12,29 @@ def count_adjacent_rolls(data: np.ndarray, a: int, b: int) -> int:
     return count
 
 
-def count_acceptable_rolls(data: np.ndarray) -> int:
+def count_acceptable_rolls(data: np.ndarray) -> tuple[int, list[tuple[int, int]]]:
     count: int = 0
     nrows, ncols = data.shape
+    indices: list[tuple[int, int]] = []
     for i in range(0, nrows):
         for j in range(0, ncols):
             if data[i, j] == "@" and count_adjacent_rolls(data, i, j) < 4:
                 count += 1
-    return count
+                indices.append((i, j))
+    return count, indices
+
+
+def count_and_remove_acceptable_rolls_iteratively(
+    data: np.ndarray,
+) -> tuple[int, np.ndarray]:
+    count: int = 0
+    count_diff: int = -1
+    while count_diff != 0:
+        count_diff, updated_indices = count_acceptable_rolls(data)
+        count += count_diff
+        for i, j in updated_indices:
+            data[i, j] = "."
+    return count, data
 
 
 if __name__ == "__main__":
@@ -27,4 +42,9 @@ if __name__ == "__main__":
         rolls_raw = f.readlines()
 
     rolls: np.ndarray = np.array([list(roll.strip()) for roll in rolls_raw])
-    print(f"{count_acceptable_rolls(rolls)=}")
+
+    removal_1 = count_acceptable_rolls(rolls)
+    removal_2 = count_and_remove_acceptable_rolls_iteratively(rolls)
+    part_1_answer: int = removal_1[0]
+    part_2_answer: int = removal_2[0]
+    print(f"{part_1_answer=}, {part_2_answer=}")
