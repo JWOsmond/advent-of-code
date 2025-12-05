3609d35d6ce85553d8db3705436c82f3380d1612
Jack Osmond <james.osmond@dhsc.gov.uk>
2025-12-05 13:03:50 +0000

    Add day 5 part 2 solution

--- 2025/day-05-cafeteria.py
+++ 2025/day-05-cafeteria.py
@@ -1,3 +1,37 @@
+import pandas as pd
+
+
+def construct_distinct_ranges(ranges_ints: list[list[int]]):
+    mins: list[int] = [range_[0] for range_ in ranges_ints]
+    maxs: list[int] = [range_[1] for range_ in ranges_ints]
+    df = (
+        pd.DataFrame({"min": mins, "max": maxs})
+        .sort_values("min")
+        .drop_duplicates()
+        .reset_index(drop=True)
+    )
+
+    # if maxima not in order having ordered by minima, then an out-of-order
+    # maximum indicates a range being a subset of the previous range
+    indices_to_keep: list[bool] = [True]
+    for idx, (min_, max_) in df[1:].iterrows():
+        if max_ <= df.loc[: (idx - 1), "max"].max():  # pyright: ignore[reportOperatorIssue]
+            indices_to_keep.append(False)
+        else:
+            indices_to_keep.append(True)
+    df = df.loc[indices_to_keep].reset_index(drop=True)
+
+    # Now, if the start of a range is less than the previous maximum, we know that the
+    # ranges overlap. Reset the start of the range to be one more than the previous
+    # maximum.
+    for idx, (min_, max_) in df.loc[1:].iterrows():
+        if min_ <= df.loc[idx - 1, "max"]:  # pyright: ignore[reportOperatorIssue]
+            df.loc[idx, "min"] = df.loc[idx - 1, "max"] + 1  # pyright: ignore[reportArgumentType, reportCallIssue, reportOperatorIssue]
+    df["range_size"] = df["max"] - df["min"] + 1
+    df = df.loc[df["range_size"] > 0]
+    return df
+
+
 if __name__ == "__main__":
     with open("2025/input/ingredients.txt") as f:
         ingredients: list[str] = [ingredient.strip() for ingredient in f.readlines()]
@@ -17,4 +51,6 @@ if __name__ == "__main__":
             if id_ >= range_[0] and id_ <= range_[1]:
                 fresh_ingredients.append(id_)
                 break
-    print(f"{len(fresh_ingredients)=}")
+
+    distinct_ranges: pd.DataFrame = construct_distinct_ranges(ranges_ints)
+    print(f"{len(fresh_ingredients)=}, {sum(distinct_ranges["range_size"])=}")
