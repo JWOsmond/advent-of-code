d5c34df7043b64335d9f1736c252437565f5a153
James Osmond <james.osmond@protonmail.com>
2025-12-06 12:46:36 +0000

    Make part 1 code more easily adaptable for part 2

--- 2025/day-06-trash-compactor.py
+++ 2025/day-06-trash-compactor.py
@@ -3,6 +3,12 @@ import re
 import numpy as np
 
 
+def process_problem_1(lines: list[str]) -> list[list[str]]:
+    return np.array(
+        [re.sub(r"\s+", " ", line.strip()).split(" ") for line in lines]
+    ).T
+
+
 def solve_problem(numbers: list[str], operation: str) -> int:
     if operation == "+":
         result: int = 0
@@ -17,13 +23,11 @@ def solve_problem(numbers: list[str], operation: str) -> int:
 
 if __name__ == "__main__":
     with open("2025/input/homework.txt") as f:
-        lines: list[str] = [line.strip() for line in f.readlines()]
+        lines: list[str] = f.readlines()
     
-    homework: list[list[str]] = np.array(
-        [re.sub(r"\s+", " ", line).split(" ") for line in lines]
-    ).T
+    homework_1: list[list[str]] = process_problem_1(lines)
     total: int = 0
-    for problem in homework:
+    for problem in homework_1:
         *numbers, operation = problem
         total += solve_problem(numbers, operation=operation)
     print(f"{total=}")
