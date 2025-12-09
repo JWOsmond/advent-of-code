3160b9af4d27b6cea495e5105e43f4c464bb6805
Jack Osmond <james.osmond@dhsc.gov.uk>
2025-12-09 09:54:55 +0000

    Day 7 part 2 possible answer

--- 2025/day-07-laboratories.py
+++ 2025/day-07-laboratories.py
@@ -1,3 +1,5 @@
+import re
+
 import numpy as np
 
 
@@ -11,33 +13,48 @@ class Beam:
     def hit_splitter(self, row: int, index: int) -> None:
         self.splits += 1
         self.tachyon_points.remove(index)
-        if index > 0 and (index - 1) not in self.tachyon_points:
-            self.manifold[row, index - 1] = "|"
-            self.tachyon_points.append(index - 1)
-        if (
-            index < (self.manifold.shape[1] - 1)
-            and (index + 1) not in self.tachyon_points
-        ):
-            self.manifold[row, index + 1] = "|"
-            self.tachyon_points.append(index + 1)
+        if index > 0:
+            self.manifold[row, index - 1] = str(
+                int(self.manifold[row - 1, index]) + int(self.manifold[row, index - 1])
+            )
+            if (index - 1) not in self.tachyon_points:
+                self.tachyon_points.append(index - 1)
+        if index < (self.manifold.shape[1] - 1):
+            self.manifold[row, index + 1] = str(
+                int(self.manifold[row - 1, index]) + int(self.manifold[row, index + 1])
+            )
+            if (index + 1) not in self.tachyon_points:
+                self.tachyon_points.append(index + 1)
 
     def traverse(self) -> None:
+        print("---------------")
         for row in range(1, self.manifold.shape[0]):
-            old_points: list[int] = self.tachyon_points.copy()
+            old_points: list[int] = sorted(self.tachyon_points.copy())
             for point in old_points:
                 # only do if point-1 >=0 etc., and ensure no duplicates
                 if self.manifold[row, point] == "^":
                     self.hit_splitter(row, point)
                 else:
-                    self.manifold[row, point] = "|"
+                    self.manifold[row, point] = (
+                        str(
+                            int(self.manifold[row, point])
+                            + int(self.manifold[row - 1, point])
+                        )
+                        if self.manifold[row - 1, point] != "S"
+                        else "1"
+                    )
+        self.number_possible_paths = sum([int(val) for val in self.manifold[-1, :]])
 
 
 if __name__ == "__main__":
     with open("2025/input/manifold.txt") as f:
         manifold_lines: list[str] = f.readlines()
 
-    manifold = np.array([list(line.strip()) for line in manifold_lines])
+    manifold = np.array(
+        [list(re.sub(r"\.", "0", line.strip())) for line in manifold_lines],
+        dtype=np.dtype("U100"),  # required so strings can be longer than 1 character
+    )
+    print(manifold)
     beam = Beam(manifold)
     beam.traverse()
-    print(beam.splits)
-    print(beam.manifold)
+    print(f"{beam.splits=}, {beam.number_possible_paths=}")
